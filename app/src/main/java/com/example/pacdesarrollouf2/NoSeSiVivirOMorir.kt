fd2cb4507cb68350327f2c4c7162231c99d3e61a
Cipriano CH <udontsi@gmail.com>
2020-11-22 12:53:58 +0100

    Corrección servicio

--- app/src/main/java/com/example/pacdesarrollouf2/NoSeSiVivirOMorir.kt
+++ app/src/main/java/com/example/pacdesarrollouf2/NoSeSiVivirOMorir.kt
@@ -1,26 +1,38 @@
 package com.example.pacdesarrollouf2
 
 import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
-import org.greenrobot.eventbus.EventBus
+import androidx.core.content.ContextCompat
 
 class NoSeSiVivirOMorir : Service(){
+    private var mp : MediaPlayer? = null
+    companion object {
+        const val CHANNEL_ID = "Musica Channel"
+        fun startService(context: Context) {
+            val startIntent = Intent(context, NoSeSiVivirOMorir::class.java)
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, NoSeSiVivirOMorir::class.java)
+            context.stopService(stopIntent)
+        }
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         createNotificationChannel()
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Reproduciendo musicota").setContentText("No se si vivir o morir!").setSmallIcon(R.drawable.ic_launcher_foreground).setContentIntent(pendingIntent).build()
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Reproduciendo musicota").setContentText("No se si vivir o morir!").setSmallIcon(R.drawable.ic_launcher_foreground).setCategory(NotificationCompat.CATEGORY_STATUS).setContentIntent(pendingIntent).build()
         startForeground(1, notification)
         Thread {
             run {
-                EventBus.getDefault().post("NoSeSiVivirOMorir")
-                val mp = MediaPlayer.create(this, R.raw.ibai)
-                mp.start()
-                stopSelf()
+                mp = MediaPlayer.create(this, R.raw.ibai)
+                mp?.start()
             }
         }.start()
         return START_NOT_STICKY
@@ -39,7 +51,10 @@ class NoSeSiVivirOMorir : Service(){
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
-    companion object {
-        const val CHANNEL_ID = "Musica Channel"
+
+    override fun onDestroy() {
+        mp?.release()
+        super.onDestroy()
     }
+
 }
